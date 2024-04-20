235894e326edc093e68fc19f08d3811ef31829ba
nimish82 <162009638+nimish82@users.noreply.github.com>
2024-04-20 17:19:10 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,30 @@
-const from = document.getElementById("from");
-const to = document.getElementById("to");
+const convertfrom = document.getElementById("from");
+const convertto = document.getElementById("to");
 const convert = document.getElementById("convert");
-const value1= document.getElementById('amount');
-const value2 = document.getElementById('to-currency');
-console.log(from.value)
+const amount= document.getElementById('amount');
+const convertedAmount = document.getElementById('to-currency');
 
 let data;
-from.onclick=async function(){
-    let response = await fetch(`https://v6.exchangerate-api.com/v6/bcbe07cb24da825602c9746f/latest/${from.value}`);
+async function fetchapi(){
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/bcbe07cb24da825602c9746f/latest/${convertfrom.value}`);
      data = await response.json();
     console.log(data['conversion_rates']);
-
-    convert.onclick=function(){
-        console.log(value1.value);
-        // console.log('sam')
-        // console.log(from.value)''
-        // value1.value= data['conversion_rates'][from.value];
-        value2.value= value1.value*data['conversion_rates'][to.value];
+    for(const key in data['conversion_rates']){
+        var option = document.createElement('option');
+        option.value= key;
+        option.text=key;
+        convertfrom.appendChild(option.cloneNode(true));
+        convertto.appendChild(option);
     }
-     
+    convert.onclick=function(){
+        convertedAmount.value= amount.value*data['conversion_rates'][convertto.value].toFixed(3);  
+    }
 }
-
+convertfrom.onchange=async function(){
+    
+    let response = await fetch(`https://v6.exchangerate-api.com/v6/bcbe07cb24da825602c9746f/latest/${convertfrom.value}`);
+     data = await response.json();
+    convert.onclick=function(){
+        convertedAmount.value= amount.value*data['conversion_rates'][convertto.value].toFixed(3);    
+    }
+}
